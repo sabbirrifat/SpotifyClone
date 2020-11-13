c3e1a99f388a5f4ad1e753a99b37a029dc48d6f8
Sabbir Rifat <rifat234dgh@gmail.com>
2020-11-13 23:55:17 +0600

    Finished the first phase

--- src/Components/Body/Body.component.jsx
+++ src/Components/Body/Body.component.jsx
@@ -2,6 +2,8 @@ import React, {Component} from 'react'
 import './body.styles.css'
 import Header from '../Header/Header.component'
 import { connect } from 'react-redux'
+import { Favorite, MoreHoriz, PlayCircleFilled } from '@material-ui/icons';
+import SongRow from "../SongRow/SongRow.component";
 
 class Body extends Component {
     render(){
@@ -17,7 +19,21 @@ class Body extends Component {
                         <h2>Discover Weekly</h2>
                         <p>{discover?.description}</p>
                     </div>
-                </div>   
+                </div>
+
+                <body className="body-songs">
+                    <body className="body-icons">
+                        <PlayCircleFilled className="body-shuffle" />
+                        <Favorite fontSize="large" />
+                        <MoreHoriz />
+                    </body>
+
+                    {
+                        discover?.tracks.items.map(item => (
+                            <SongRow track= {item.track}/>
+                        ))}
+                </body>
+
             </div>
         )
     }
